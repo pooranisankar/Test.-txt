da3df98f7b11cbc407c19a69aac4eed48ebc335b
pooranisankar <pooranisankar888@gmail.com>
2025-07-29 17:19:33 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int a=5;
-    int b=6;
-    int sum=a+b;
-    cout<<sum<<endl;
+
+int main() {
+    vector<int> nums = {1, 2, 3, 4};
+    for (int i = 1; i < nums.size(); ++i)
+        nums[i] += nums[i - 1];
+    for (int n : nums) cout << n << " ";
 }
